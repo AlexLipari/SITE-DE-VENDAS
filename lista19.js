fa483bac7c70765657501e37125d2a4418d46c84
Alexandre Lipari <santuslipari@gmail.com>
2022-09-01 16:20:49 -0300

    Função para buscar produto pelo ID e exibir informações, obs, correção linha 12

--- lista19.js
+++ lista19.js
@@ -3,11 +3,26 @@ var arrayNome = []
 var arrayPreco = []
 var arrayAvaliacao = []
 var contador = 0
-cadastrarProduto ()
-function cadastrarProduto (){
+cadastrarProduto()
+var ID = prompt("Digite o ID do produto")
+buscarProduto(ID)
+
+function cadastrarProduto() {
     arrayId[contador] = parseInt(prompt("Inserir um ID:"))
-    arrayNome[contador] = parseInt(prompt("Inserir nome do produto:"))
+    arrayNome[contador] = prompt("Inserir nome do produto:")
     arrayPreco[contador] = parseFloat(prompt("Inserir preço:"))
     arrayAvaliacao[contador] = parseInt(prompt("Inserir avaliação"))
     contador++
+}
+
+function buscarProduto(idParametro) {
+
+    for (var index = 0; index < contador; index++) {
+        if (idParametro == arrayId[index]) {
+
+            console.log(arrayId[index], arrayNome[index], arrayPreco[index], arrayAvaliacao[index])
+        }
+    }
+
+
 }
