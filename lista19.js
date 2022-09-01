451d39ac69c6b4a44fcdf69783095427ed10b49c
Alexandre Lipari <santuslipari@gmail.com>
2022-09-01 18:44:25 -0300

    Função para buscarpro

--- lista19.js
+++ lista19.js
@@ -4,8 +4,10 @@ var arrayPreco = []
 var arrayAvaliacao = []
 var contador = 0
 cadastrarProduto()
-var ID = prompt("Digite o ID do produto")
-buscarProduto(ID)
+var ID = prompt("Digite o ID do produto:")
+var nome = prompt("Digite o nome do produto:")
+buscarId(ID)
+ console.log (buscarNome(nome)) 
 
 function cadastrarProduto() {
     arrayId[contador] = parseInt(prompt("Inserir um ID:"))
@@ -15,14 +17,23 @@ function cadastrarProduto() {
     contador++
 }
 
-function buscarProduto(idParametro) {
+function buscarId(idParametro) {
 
     for (var index = 0; index < contador; index++) {
         if (idParametro == arrayId[index]) {
 
             console.log(arrayId[index], arrayNome[index], arrayPreco[index], arrayAvaliacao[index])
+
+        }
+    }
+}
+function buscarNome(nomeParametro) {
+    for (var index = 0; index < contador; index++) {
+        if (nomeParametro == arrayNome[index] ) {
+            console.log(arrayNome[index])
+            return arrayId[index]
         }
     }
 
-
 }
+
