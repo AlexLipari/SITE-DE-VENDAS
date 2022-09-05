85374fed7b9fd1a62c9a123a56cd3f63c4df8278
Alexandre Lipari <santuslipari@gmail.com>
2022-09-05 20:58:48 -0300

    Funçao deletar um produto e organizar os arrays para nao deixar espaços vazios

--- lista19.js
+++ lista19.js
@@ -20,7 +20,15 @@ ordenadosPeloId()
 exibirProdutos()
 console.log("  ")
 ordenarPelaAvaliacao()
+exibirProdutos()
+console.log("  ")
+ordenarPelaAvaliacao()
+exibirProdutos()
+console.log("  ")
 atualizarPreco(idTrocaValor, trocarValor)
+exibirProdutos()
+console.log("  ")
+deletarProduto()
 
 
 function cadastrarProduto() {
@@ -72,10 +80,51 @@ function exibirProdutos() {
 function atualizarPreco(idParametro, novoValor) {
     for (var index = 0; index < contador; index++) {
         if (idParametro == arrayId[index]) {
-            arrayPreco[index] = novoValor   
+            arrayPreco[index] = novoValor
         }
 
     }
+    exibirProdutos()
+}
+function deletarProduto(idParametro) {
+    for (var index = 0; index < contador; index++) {
+        if (idParametro == arrayId[index])
+            arrayId[index] = 0
+        arrayNome[index] = 0
+        arrayPreco[index] = 0
+        arrayAvaliacao[index] = 0
+    }
+
+    for (var atual = 0; atual < contador - 1; atual++) {
+        for (var seguinte = atual + 1; seguinte < contador; seguinte++) {
+            if (arrayId[atual] == 0) {
+                arrayId[atual] = arrayId[seguinte]
+                arrayId[seguinte] = 0
+                arrayNome[atual] = arrayNome[seguinte]
+                arrayNome[seguinte] = 0
+                arrayPreco[atual] = arrayPreco[seguinte]
+                arrayPreco[seguinte] = 0
+                arrayAvaliacao[atual] = arrayAvaliacao[seguinte]
+                arrayAvaliacao[seguinte] = 0
+            }
+
+        }
+    }
+    var arrayIdAux = []
+    var arrayNomeAux = []
+    var arrayPrecoAux = []
+    var arrayAvaliacaoAux = []
+    for (var index = 0; index < contador; index++) {
+        if (arrayId[index] != 0) {
+            arrayIdAux[index] = arrayId[index]
+            arrayNomeAux[index] = arrayNome[index]
+            arrayPrecoAux[index] = arrayPreco[index]
+            arrayAvaliacaoAux[index] = arrayAvaliacao[index]
+        }
+
+    }
+    contador--
+    arrayId = arrayIdAux
     exibirProdutos()
 }
 function ordenar(propriedadesParametro) {
