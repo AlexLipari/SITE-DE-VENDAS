e7bce177a73c7827e1802b5f140c14cc97ca209a
Alexandre Lipari <santuslipari@gmail.com>
2022-09-05 16:10:34 -0300

    Função para ordenar ID apresentando produtos

--- lista19.js
+++ lista19.js
@@ -4,11 +4,17 @@ var arrayPreco = []
 var arrayAvaliacao = []
 var contador = 0
 cadastrarProduto()
+cadastrarProduto()
+cadastrarProduto()
+cadastrarProduto()
+
 var ID = prompt("Digite o ID do produto:")
 var nome = prompt("Digite o nome do produto:")
 buscarId(ID)
- console.log (buscarNome(nome)) 
-
+console.log(buscarNome(nome))
+exibirProdutos()
+console.log("  ")
+ordenadosPeloId ()
 function cadastrarProduto() {
     arrayId[contador] = parseInt(prompt("Inserir um ID:"))
     arrayNome[contador] = prompt("Inserir nome do produto:")
@@ -29,7 +35,7 @@ function buscarId(idParametro) {
 }
 function buscarNome(nomeParametro) {
     for (var index = 0; index < contador; index++) {
-        if (nomeParametro == arrayNome[index] ) {
+        if (nomeParametro == arrayNome[index]) {
             console.log(arrayNome[index])
             return arrayId[index]
         }
@@ -37,3 +43,30 @@ function buscarNome(nomeParametro) {
 
 }
 
+function ordenadosPeloId() {
+    for (var atual = 0; atual < contador - 1; atual++) {
+        for (var seguinte = atual + 1; seguinte < contador; seguinte++) {
+            var idAux = arrayId[atual]
+            var nomeAux = arrayNome[atual]
+            var precoAux = arrayPreco[atual]
+            var avaliacaoAux = arrayAvaliacao[atual]
+            if (arrayId[atual] > arrayId[seguinte]) {
+                arrayId[atual] = arrayId[seguinte]
+                arrayId[seguinte] = idAux
+                arrayNome[atual] = arrayNome[seguinte]
+                arrayNome[seguinte] = nomeAux
+                arrayPreco[atual] = arrayPreco[seguinte]
+                arrayPreco[seguinte] = precoAux
+                arrayAvaliacao[atual] = arrayAvaliacao[seguinte]
+                arrayAvaliacao[seguinte] = avaliacaoAux
+            }
+
+        }
+    }
+    exibirProdutos ()
+}
+function exibirProdutos (){
+    for(var index=0;index<contador;index++){
+        console.log(arrayId(index),arrayNome[index],arrayPreco[index],arrayAvaliacao[index])
+    }
+}
